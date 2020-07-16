def1b7f5aef8f8c53c360c8ce20cc176bbdaf0a6
HungDQ <hung.do.quang@ntq-solution.com.vn>
2020-07-16 10:05:49 +0700

    add sample

---
+++ sample-to-captupe/PULL_UP_METHOD.js
@@ -0,0 +1,21 @@
+// ======> BEFORE
+
+class Employee {...}
+
+class Salesman extends Employee {
+    get name() {...}
+}
+
+class Engineer extends Employee {
+    get name() {...}
+}
+
+// ======> AFTER
+
+class Employee {
+    get name() {...}
+}
+
+class Salesman extends Employee {...}
+
+class Engineer extends Employee {...}
